a393f7ed752041dbd78bd765c60830999ce4bdc9
Hyunkyu Kim <hyunkyu001@gmail.com>
2021-09-15 23:47:59 +0900

    210915 django

--- django/210901_django_practice/articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'articles'   # templates/articles와 같이 해줄 떄 같이
-
-urlpatterns = [
-    path('index/', views.index),
-    path('', views.index),  # ''는 기본주소 -> https://127.0.0.1:8000/articles/ 주소를 의미
-]
--- django/django_practice/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def dinner(request, menu, number):
-    context = {
-        'menu': menu,
-        'number': number,
-    }
-    return render(request, 'dinner.html', context)
