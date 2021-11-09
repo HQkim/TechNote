65877099d95e3a23fa6751b55ea26c93edce87ea
Hyunkyu Kim <hyunkyu001@gmail.com>
2021-11-10 00:56:19 +0900

    211110 vuejs_vuex_todolist

---
+++ vuejs/04_vuejs_vuex_todolist/src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <todo-list-item v-for="todo in todos" :key="todo.date" :todo="todo">
+    </todo-list-item>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import TodoListItem from "@/components/TodoListItem";
+
+export default {
+  name: "TodoList",
+  components: {
+    TodoListItem,
+  },
+  // 1.
+  // computed: {
+  //   todos: function () {
+  //     return this.$store.state.todos;
+  //   },
+  // },
+  // 2 이러면 다른 computed를 쓸 수 없음
+  // computed: mapState(["todos"]),
+  // 3. 최종
+  computed: {
+    ...mapState(["todos"]),
+  },
+};
+</script>
+
+<style>
+</style>
